43bfb5d7c1db47015a269fc852d70fcefeb0eb02
v-zim <110477319+v-zim@users.noreply.github.com>
2026-05-13 23:12:29 +0000

    teste no editor do streamlit 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,24 @@ from pathlib import Path
 import plotly
 import plotly.express as px
 import plotly.io as pio
+import polars as pl
 
 PASTA_PARQUETS = Path("parquets_remoto")
 
-print(Path.cwd())
-print(list(Path(".").iterdir()))
+# print(Path.cwd())
+# print(list(Path(".").iterdir()))
+
+lista_parquets = list(PASTA_PARQUETS.glob("*.parquet"))
+parquets = [p.name.split(".")[0] for p in lista_parquets]
 
 # Plotar no app
 
-st.title("teste teste")
-value = st.slider("alo", 0, 100)
-st.write("valor:", value)
+st.title("Eventos climáticos")
+escolhido = st.selectbox("Selecione o arquivo desejado", parquets)
+
+caminho_arquivo = f"{PASTA_PARQUETS}/{escolhido}.parquet"
+df = pl.read_parquet(caminho_arquivo)
 
 # pio.renderers.default = "browser"
-# fig = px.bar(data_frame=df_por_estacao, x='PERIODO', y='PRECIPITACAO TOTAL, HORARIO (mm)', color='ESTACAO DO ANO')
+fig = px.bar(data_frame=df, x='PERIODO', y='PRECIPITACAO TOTAL, HORARIO (mm)', color='ESTACAO DO ANO')
+st.plotly_chart(fig)
